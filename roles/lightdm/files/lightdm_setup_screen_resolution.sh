45763494eb1316e9da8da7b0928b56a1c9337d19
Giorgio Gambino <kepler@Giorgios-MacBook-Pro.local>
2020-11-01 22:35:54 +0000

    fix lightdm screen resolution

--- roles/lightdm/files/lightdm_setup_screen_resolution.sh
+++ roles/lightdm/files/lightdm_setup_screen_resolution.sh
@@ -1,2 +1,23 @@
 #!/bin/sh
-xrandr --output Virtual-1 --primary --mode 1920x1200
+set -x
+
+output="$1"
+x="$2"
+y="$3"
+frequency="$4"
+
+if [ $# -ne 4 ]; then
+echo "Usage: $0 output x y frequency"
+echo "To find output name: xrandr -q"
+exit 0
+fi
+
+mode=$( cvt "$x" "$y" "$frequency" | grep -v '^#' | cut -d' ' -f3- )
+modename="${x}x${y}"
+
+xrandr --newmode $modename $mode
+xrandr --addmode "$output" "$modename"
+xrandr --output "$output" --mode "$modename"
+
+# Always return success or lightdm goes into infinite loop
+exit 0
